b811b73ba616b81a9e204b4756eabf86c1db5515
Chris Crow and Mel Yasuda <mel@>
2018-09-12 09:24:31 -0700

    add RESTful routes.

--- TaskList/Controllers/ItemsController.cs
+++ TaskList/Controllers/ItemsController.cs
@@ -26,9 +26,15 @@ namespace TaskList.Controllers
         public ActionResult Create()
         {
             Item newItem = new Item(Request.Form["new-item"]);
-            newItem.Save();
             List<Item> allItems = Item.GetAll();
             return View("Index", allItems);
+        }
+
+        [HttpGet("/items/{id}")]
+        public ActionResult Details(int id)
+        {
+            Item item = Item.Find(id);
+            return View(item);
         }
     }
 }
